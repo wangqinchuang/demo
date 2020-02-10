d5fd7cc36fc04ecfa602a9788644d6d273d3132c
wangqinchuang <704794278@qq.com>
2020-02-10 21:04:39 +0800

    hello world

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,7 @@ public class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("HelloWorld");
 		System.out.println("1");
+		System.out.println("HelloWorld");
+		System.out.println("2");
 	}
 }
